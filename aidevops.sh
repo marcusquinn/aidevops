c6833295d022b468ef8262e6f165610d485c602a
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2026-06-18 01:59:45 +0100

    wip: fix trailing newline helper

--- aidevops.sh
+++ aidevops.sh
@@ -163,11 +163,16 @@ check_file() {
 ensure_trailing_newline() {
 	local file="$1"
 	local last
-	last="$(
-		tail -c 1 "$file"
-		printf x
-	)"
-	[[ -s "$file" ]] && [[ "$last" != $'\n'x ]] && printf '\n' >>"$file"
+	if [[ -s "$file" ]]; then
+		last="$(
+			tail -c 1 "$file"
+			printf x
+		)"
+		if [[ "$last" != $'\n'x ]]; then
+			printf '\n' >>"$file"
+		fi
+	fi
+	return 0
 }
 
 # Source CLI implementation modules from the namespaced module tree.
