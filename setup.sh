31a2423a98f40a110d3b4e45c4b1c51ec0d89d1c
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2026-09-22 03:16:43 +0100

    chore(release): bump version to 3.34.14

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ fi
 # AI Assistant Server Access Framework Setup Script
 # Helps developers set up the framework for their infrastructure
 #
-# Version: 3.34.13
+# Version: 3.34.14
 #
 # Quick Install:
 #   npm install -g aidevops && aidevops update          (recommended)
